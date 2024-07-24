85b9408e810c51a1f60165bba0491102c577f11f
meiers14 <meiers14@outlook.de>
2024-07-24 21:54:10 +0200

    Updated general angular app configuration, Started implementing home component methods

--- angular-frontend/src/app/home/home.component.ts
+++ angular-frontend/src/app/home/home.component.ts
@@ -2,11 +2,21 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  standalone: true,
-  imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  playerName: string = '';
+  lobbyCode: string = '';
+  otherPlayerName: string = '';
+  players: string[] = []; // Beispiel für ngFor
 
+  validatePlayerName() : boolean {
+    // Returns true if the entered username is unique
+    return this.playerName != this.otherPlayerName;
+  }
+
+  joinFriend() {}
+
+  createNewGame() {}
 }
